cc277aabe7e1dee2c57d364b9cc38a31e7f9029c
TANIGUCHI Masaya <ta2gch@gmail.com>
2026-02-22 10:10:16 +0900

    Refactor Repl.Executor in a flatter Haskell style

--- src/Repl/Executor.hs
+++ src/Repl/Executor.hs
@@ -9,6 +9,11 @@ module Repl.Executor (
 import qualified Prelude ()
 import MHSPrelude
 
+import Control.Applicative ((<|>))
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Except (ExceptT(..), runExceptT)
+import Data.Maybe (isJust, listToMaybe, mapMaybe)
+
 import MicroHs.Ident (mkIdent)
 import MicroHs.SymTab (stLookup, Entry(..))
 import MicroHs.Expr (showExpr)
@@ -25,95 +30,64 @@ data MetaCommand
   = TypeOfExpr String
   | KindOfType String
 
+data SplitPlan
+  = SplitDefineOnly String
+  | SplitDefineThenRun String String
+
+data Completion
+  = Complete
+  | Incomplete
+  | Invalid
+
 replDefine :: ReplCtx -> String -> IO (Either ReplError ReplCtx)
-replDefine ctx snippet = do
-  let snippet' = ensureTrailingNewline snippet
-  case appendDefinition ctx snippet' of
-    Left err -> pure (Left err)
-    Right defsWithNew -> do
-      cm <- compileModule ctx (moduleFromDefs defsWithNew)
-      case cm of
-        Left err'          -> pure (Left err')
-        Right (_, cache', syms') -> pure (Right ctx{ rcDefs = defsWithNew, rcCache = cache', rcSyms = syms' })
+replDefine ctx snippet = runExceptT $ do
+  let snippetWithNewline = ensureTrailingNewline snippet
+  defsWithNew <- ExceptT (pure (appendDefinition ctx snippetWithNewline))
+  (_, cache', syms') <- ExceptT (compileModule ctx (moduleFromDefs defsWithNew))
+  pure ctx{ rcDefs = defsWithNew, rcCache = cache', rcSyms = syms' }
 
 replRun :: ReplCtx -> String -> IO (Either ReplError ReplCtx)
-replRun ctx stmt = do
+replRun ctx stmt = runExceptT $ do
   let block = runBlock stmt
       src = moduleSourceWith ctx block
-  cm <- compileModule ctx src
-  case cm of
-    Left err -> pure (Left err)
-    Right (cmdl, cache', syms') -> do
-      r <- runAction cache' cmdl runResultIdent
-      case r of
-        Left e  -> pure (Left e)
-        Right _ -> pure (Right ctx{ rcCache = cache', rcSyms = syms' })
+  (cmdl, cache', syms') <- ExceptT (compileModule ctx src)
+  _ <- ExceptT (runAction cache' cmdl runResultIdent)
+  pure ctx{ rcCache = cache', rcSyms = syms' }
 
 replIsComplete :: ReplCtx -> String -> IO String
-replIsComplete ctx snippet = do
-  if all isws snippet then pure "complete" else do
-    let ls = lines (ensureTrailingNewline snippet)
-        go n
-          | n < 0 = pure "invalid"
-          | otherwise = do
-              let (defLines, runLines) = splitAt n ls
-                  defPart = unlines defLines
-                  runPart = unlines (dropWhileEnd allwsLine runLines)
-                  candidateDefs = currentDefsSource ctx ++ defPart
-              if canParseDefinition candidateDefs
-                then if all allwsLine runLines
-                     then pure "complete"
-                     else if canParseExpression runPart || canParseExpression ("do\n" ++ indent runPart)
-                          then pure "complete"
-                          else go (n - 1)
-                else go (n - 1)
-    res <- go (length ls)
-    if res == "invalid" && isIncomplete snippet
-      then pure "incomplete"
-      else pure res
+replIsComplete ctx snippet = pure (toText completion)
+  where
+    completion
+      | all isws snippet = Complete
+      | hasValidSplit = Complete
+      | isIncomplete snippet = Incomplete
+      | otherwise = Invalid
+
+    hasValidSplit = isJust (firstValidSplitPlan ctx (lines (ensureTrailingNewline snippet)))
+
+    toText state =
+      case state of
+        Complete -> "complete"
+        Incomplete -> "incomplete"
+        Invalid -> "invalid"
 
 replInspect :: ReplCtx -> String -> IO (Either ReplError String)
 replInspect ctx name = do
   let ident = mkIdent name
       (typeTable, valueTable) = rcSyms ctx
-  case stLookup "value" ident valueTable of
-    Right (Entry _ sigma) -> return (Right $ name ++ " :: " ++ showExpr sigma)
-    Left _ -> case stLookup "type" ident typeTable of
-      Right (Entry _ kind) -> return (Right $ name ++ " :: " ++ showExpr kind)
-      Left _ -> return (Left $ ReplRuntimeError $ "Identifier not found: " ++ name)
+      valueHit = lookupRendered "value" ident valueTable name
+      typeHit = lookupRendered "type" ident typeTable name
+      notFound = ReplRuntimeError ("Identifier not found: " ++ name)
+  pure (maybe (Left notFound) Right (valueHit <|> typeHit))
 
 replExecute :: ReplCtx -> String -> IO (Either ReplError ReplCtx)
 replExecute ctx snippet
   | Just cmd <- parseMetaCommand snippet = runMetaCommand ctx cmd
-replExecute ctx snippet = do
-  let ls = lines (ensureTrailingNewline snippet)
-      go n
-        | n < 0 = pure $ Left (ReplParseError "unable to parse snippet")
-        | otherwise = do
-            let (defLines, runLines) = splitAt n ls
-                defPart = unlines defLines
-                runPart = unlines (dropWhileEnd allwsLine runLines)
-                candidateDefs = currentDefsSource ctx ++ defPart
-            if canParseDefinition candidateDefs
-              then if all allwsLine runLines
-                   then replDefine ctx defPart
-                   else if canParseExpression runPart
-                        then do
-                          eCtx' <- replDefine ctx defPart
-                          case eCtx' of
-                            Left err -> pure (Left err)
-                            Right ctx' -> replRun ctx' runPart
-                        else
-                          let runBlock = "do\n" ++ indent runPart
-                          in if canParseExpression runBlock
-                             then do
-                               eCtx' <- replDefine ctx defPart
-                               case eCtx' of
-                                 Left err -> pure (Left err)
-                                 Right ctx' -> replRun ctx' runBlock
-                             else go (n - 1)
-              else go (n - 1)
-  go (length ls)
+replExecute ctx snippet =
+  case firstValidSplitPlan ctx (lines (ensureTrailingNewline snippet)) of
+    Nothing -> pure (Left (ReplParseError "unable to parse snippet"))
+    Just (SplitDefineOnly defPart) -> replDefine ctx defPart
+    Just (SplitDefineThenRun defPart runPart) -> defineThenRun ctx defPart runPart
 
 runMetaCommand :: ReplCtx -> MetaCommand -> IO (Either ReplError ReplCtx)
 runMetaCommand ctx cmd =
@@ -123,64 +97,59 @@ runMetaCommand ctx cmd =
 
 replTypeOf :: ReplCtx -> String -> IO (Either ReplError ReplCtx)
 replTypeOf ctx expr =
-  let expr' = trimWs expr
-      probeName = "xhReplTypeProbe"
-      probeIdent = mkIdent probeName
-      probeDef = unlines
-        [ probeName ++ " = ("
-        , indent expr'
-        , "  )"
-        ]
-      src = moduleSourceWith ctx probeDef
-  in if null expr'
-       then pure (Left (ReplRuntimeError ":type expects an expression"))
-       else do
-         cm <- compileModule ctx src
-         case cm of
-           Left err -> pure (Left err)
-           Right (_, _, (_, valueTable)) ->
-             case stLookup "value" probeIdent valueTable of
-               Right (Entry _ sigma) -> do
-                 putStrLn ("> " ++ expr' ++ " :: " ++ showExpr sigma)
-                 pure (Right ctx)
-               Left _ ->
-                 pure (Left (ReplRuntimeError "failed to infer expression type"))
+  runProbe ctx
+    ":type expects an expression"
+    "failed to infer expression type"
+    trimmedExpr
+    src
+    inferType
+  where
+    trimmedExpr = trimWs expr
+    probeName = "xhReplTypeProbe"
+    probeIdent = mkIdent probeName
+    probeDef = unlines
+      [ probeName ++ " = ("
+      , indent trimmedExpr
+      , "  )"
+      ]
+    src = moduleSourceWith ctx probeDef
+    inferType (_, valueTable) =
+      case stLookup "value" probeIdent valueTable of
+        Right (Entry _ sigma) -> Just (showExpr sigma)
+        Left _ -> Nothing
 
 replKindOf :: ReplCtx -> String -> IO (Either ReplError ReplCtx)
 replKindOf ctx ty =
-  let ty' = trimWs ty
-      probeName = "XhReplKindProbe"
-      probeIdent = mkIdent probeName
-      probeDef = "type " ++ probeName ++ " = " ++ ty' ++ "\n"
-      src = moduleSourceWith ctx probeDef
-  in if null ty'
-       then pure (Left (ReplRuntimeError ":kind expects a type"))
-       else do
-         cm <- compileModule ctx src
-         case cm of
-           Left err -> pure (Left err)
-           Right (_, _, (typeTable, _)) ->
-             case stLookup "type" probeIdent typeTable of
-               Right (Entry _ kind) -> do
-                 putStrLn ("> " ++ ty' ++ " :: " ++ showExpr kind)
-                 pure (Right ctx)
-               Left _ ->
-                 pure (Left (ReplRuntimeError "failed to infer type kind"))
+  runProbe ctx
+    ":kind expects a type"
+    "failed to infer type kind"
+    trimmedType
+    src
+    inferKind
+  where
+    trimmedType = trimWs ty
+    probeName = "XhReplKindProbe"
+    probeIdent = mkIdent probeName
+    probeDef = "type " ++ probeName ++ " = " ++ trimmedType ++ "\n"
+    src = moduleSourceWith ctx probeDef
+    inferKind (typeTable, _) =
+      case stLookup "type" probeIdent typeTable of
+        Right (Entry _ kind) -> Just (showExpr kind)
+        Left _ -> Nothing
 
 parseMetaCommand :: String -> Maybe MetaCommand
 parseMetaCommand raw =
-  let snippet = trimWs raw
-  in case parseWith ":type" TypeOfExpr snippet of
-       Just cmd -> Just cmd
-       Nothing -> parseWith ":kind" KindOfType snippet
+  parseWith ":type" TypeOfExpr snippet <|> parseWith ":kind" KindOfType snippet
   where
-    parseWith keyword ctor snippet =
-      if startsWith keyword snippet && hasBoundary keyword snippet
-        then Just (ctor (drop (length keyword) snippet))
+    snippet = trimWs raw
+
+    parseWith keyword ctor s =
+      if startsWith keyword s && hasBoundary keyword s
+        then Just (ctor (drop (length keyword) s))
         else Nothing
 
-    hasBoundary keyword snippet =
-      let rest = drop (length keyword) snippet
+    hasBoundary keyword s =
+      let rest = drop (length keyword) s
       in null rest || isws (head rest)
 
 startsWith :: String -> String -> Bool
@@ -188,3 +157,52 @@ startsWith prefix s = take (length prefix) s == prefix
 
 trimWs :: String -> String
 trimWs = dropWhile isws . dropWhileEnd isws
+
+classifySplit :: ReplCtx -> [String] -> Int -> Maybe SplitPlan
+classifySplit ctx snippetLines splitIndex
+  | not (canParseDefinition candidateDefs) = Nothing
+  | all allwsLine runLines = Just (SplitDefineOnly defPart)
+  | canParseExpression runPart = Just (SplitDefineThenRun defPart runPart)
+  | canParseExpression doRunPart = Just (SplitDefineThenRun defPart doRunPart)
+  | otherwise = Nothing
+  where
+    (defLines, runLines) = splitAt splitIndex snippetLines
+    defPart = unlines defLines
+    runPart = unlines (dropWhileEnd allwsLine runLines)
+    doRunPart = "do\n" ++ indent runPart
+    candidateDefs = currentDefsSource ctx ++ defPart
+
+firstValidSplitPlan :: ReplCtx -> [String] -> Maybe SplitPlan
+firstValidSplitPlan ctx snippetLines =
+  listToMaybe (mapMaybe (classifySplit ctx snippetLines) [length snippetLines, length snippetLines - 1 .. 0])
+
+defineThenRun :: ReplCtx -> String -> String -> IO (Either ReplError ReplCtx)
+defineThenRun ctx defPart runPart = runExceptT $ do
+  ctx' <- ExceptT (replDefine ctx defPart)
+  ExceptT (replRun ctx' runPart)
+
+runProbe
+  :: ReplCtx
+  -> String
+  -> String
+  -> String
+  -> String
+  -> (Symbols -> Maybe String)
+  -> IO (Either ReplError ReplCtx)
+runProbe ctx emptyInputError inferError shown src infer = runExceptT $ do
+  shown' <- ExceptT (pure $
+    if null shown
+      then Left (ReplRuntimeError emptyInputError)
+      else Right shown)
+  (_, _, syms) <- ExceptT (compileModule ctx src)
+  inferred <- maybe
+    (ExceptT (pure (Left (ReplRuntimeError inferError))))
+    pure
+    (infer syms)
+  lift (putStrLn ("> " ++ shown' ++ " :: " ++ inferred))
+  pure ctx
+
+lookupRendered scope ident table shown =
+  case stLookup scope ident table of
+    Right (Entry _ sigOrKind) -> Just (shown ++ " :: " ++ showExpr sigOrKind)
+    Left _ -> Nothing
